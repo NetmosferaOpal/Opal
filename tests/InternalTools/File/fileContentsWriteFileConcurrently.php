3dc371e8b2f6393547568fd70d5272d15e60c778
Netmosfera <netmosfera@gmail.com>
2019-02-10 11:45:44 +0100

    no message

--- tests/InternalTools/File/fileContentsWriteFileConcurrently.php
+++ tests/InternalTools/File/fileContentsWriteFileConcurrently.php
@@ -4,7 +4,8 @@ ini_set("xdebug.overload_var_dump", "0");
 
 (function($fileName){
     var_dump($file = fopen(__DIR__ . "/" . $fileName, "c"));
-    var_dump(flock($file, LOCK_EX | LOCK_NB));
+    var_dump(flock($file, LOCK_EX | LOCK_NB, $wouldBlock));
+    var_dump($wouldBlock);
     var_dump(ftruncate($file, 0));
     var_dump(fwrite($file, "baz qux"));
     var_dump(flock($file, LOCK_UN));
