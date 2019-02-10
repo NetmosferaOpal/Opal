6031ac358f39b90096cbbc41ad2b64399f3c7345
Netmosfera <netmosfera@gmail.com>
2019-02-11 00:59:49 +0100

    Improve fileWrite()

--- composer-includes.php
+++ composer-includes.php
@@ -6,6 +6,6 @@ require(__DIR__ . "/src/InternalTools/componentFromTypeName.inc.php");
 require(__DIR__ . "/src/InternalTools/isValidIdentifier.inc.php");
 require(__DIR__ . "/src/InternalTools/isValidIdentifiers.inc.php");
 require(__DIR__ . "/src/InternalTools/File/fileRead.inc.php");
+require(__DIR__ . "/src/InternalTools/File/fileWrite.inc.php");
 require(__DIR__ . "/src/InternalTools/File/isAbsolutePath.inc.php");
-require(__DIR__ . "/src/InternalTools/File/requireNewPHPFile.inc.php");
 require(__DIR__ . "/src/InternalTools/File/retryWithinTimeLimit.inc.php");
--- src/InternalTools/File/fileWrite.inc.php
+++ src/InternalTools/File/fileWrite.inc.php
@@ -1,58 +1,57 @@
 <?php declare(strict_types = 1);
 
 namespace Netmosfera\Opal\InternalTools\File;
+use Closure;
 
 /**
  * @TODOC
  *
  * @param           String $path
+ * `String`
  *
- * @param           String $phpSource
+ * @param           String $contents
+ * `String`
  *
  * @param           Int $directoryMode
+ * `Int`
  *
  * @param           Float $maxTimeInSeconds
+ * `Float`
  *
  * @param           Float $retryDelayInSeconds
+ * `Float`
  *
- * @param           Bool $requireIt
+ * @param           Closure|NULL $callback
+ * `Closure0` Called after the file has been written, but while it is still locked.
  *
  * @returns         Bool
+ * `Bool`
  */
-function requireNewPHPFile(
+function fileWrite(
     String $path,
-    String $phpSource,
+    String $contents,
     Int $directoryMode,
     Float $maxTimeInSeconds,
     Float $retryDelayInSeconds,
-    Bool $requireIt
+    ?Closure $callback = NULL
 ): Bool{
     assert(isAbsolutePath($path));
-
     $directory = dirname($path);
-
     return retryWithinTimeLimit(function() use(
-        $directory,
-        $directoryMode,
-        $path,
-        $phpSource,
-        $requireIt
+        &$directory, &$directoryMode, &$path, &$contents, &$callback
     ){
         $saveUMask = umask(0);
         @mkdir($directory, $directoryMode, TRUE);
         @umask($saveUMask);
         $file = @fopen($path, "c");
-        if($file === FALSE){ return FALSE; }
+        if($file === FALSE) return FALSE;
+        // @TODO this should also set the permissions to the file using umask
         $lockAcquired = flock($file, LOCK_EX | LOCK_NB);
-        if($lockAcquired === FALSE){ return FALSE; }
+        if($lockAcquired === FALSE) return FALSE;
         ftruncate($file, 0);
-        fwrite($file, $phpSource);
+        fwrite($file, $contents);
         fflush($file);
-        if($requireIt){
-            (function($__OPAL_FILE__){
-                require $__OPAL_FILE__;
-            })($path);
-        }
+        if($callback !== NULL) $callback();
         flock($file, LOCK_UN);
         fclose($file);
         return TRUE;
