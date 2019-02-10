e1e5b8e6a954e9eb6a715353447de7c2f6b5e597
Netmosfera <netmosfera@gmail.com>
2019-02-10 07:58:36 +0100

    Improve test.

--- tests/InternalTools/ReproBugTest.php
+++ tests/InternalTools/ReproBugTest.php
@@ -8,22 +8,23 @@ class ReproBugTest extends TestCase
         // create a file of 6 bytes
         $path = __DIR__ . "/foo.txt";
         file_put_contents($path, "123456");
+
         // (1)
         // open that file for read and shared-lock it
         $file = fopen($path, "r");
         flock($file, LOCK_SH | LOCK_NB);
+
         // (2)
         // attempt to write that file concurrently while the lock is active
-        var_dump(`php -r "var_dump(file_put_contents('$path', 'baz')); echo 123;"`);
+        echo "bytes written: ";
+        echo `php -r "var_dump(file_put_contents('$path', 'baz'));"` . "\n";
         // this step fails as expected -- means the file is still "123456"
+
         // (3)
         // attempt to read the file's size
         clearstatcache(FALSE, $path);
         $fileSize = filesize($path);
-        // should return 6 bytes but returns 0
-        // (4)
-        // this fails as consequence
-        echo $fileSize . "\n";
-        echo fread($file, $fileSize);
+        echo "file-size: " . $fileSize . "\n";
+        echo "contents: " . fread($file, $fileSize) . "\n";
     }
 }
