4adf924df622b995603f97df82755e050b558e20
Netmosfera <netmosfera@gmail.com>
2019-02-11 01:01:20 +0100

    Remove unnecessary ini_set()

--- tests/InternalTools/File/fileRead_readFileConcurrently.php
+++ tests/InternalTools/File/fileRead_readFileConcurrently.php
@@ -1,7 +1,5 @@
 <?php declare(strict_types = 1);
 
-ini_set("xdebug.overload_var_dump", "0");
-
 (function($fileName){
     $file = fopen(__DIR__ . "/" . $fileName, "c");
     echo flock($file, LOCK_SH | LOCK_NB, $wouldBlock) ? "TRUE" : "FALSE", "\n";
